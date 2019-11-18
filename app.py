032f1ac7a7fc5567e7d42ab4988fb974ae2d1947
Zhekai Hu <kryptonz16@gmail.com>
2019-11-18 15:47:56 -0800

    update

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def cal():
-    if request.method == 'POST':
+    if request.method == 'GET':
+    	return render_template('index.html')
+    elif request.method == 'POST':
+
         allotment = request.form['allotment']
         final = request.form['final']
         sell_commission = request.form['sell_commission']
@@ -44,8 +49,9 @@ def cal():
         print_break_even_price = "$" + "%.2f" % break_even_price
 
         return render_template('result.html', proceeds=print_proceeds, cost=print_cost, allotment=allotment, initial=print_initial, initial_total=print_initial_total, buy_commission=print_buy_commission, sell_commission=print_sell_commission, tax=print_tax, capital_gain=print_capital_gain, tax_on_capital_gain=print_tax_on_capital_gain, net_profit=print_net_profit, return_on_investment=print_return_on_investment, break_even_price=print_break_even_price)
-    return render_template('index.html')
+    
 
 
-if __name__ == "main":
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=80)
+
